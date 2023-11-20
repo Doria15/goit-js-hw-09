1d3c4b7579aad69a1f5b20a92bd03fed39631755
Doria15 <doriaceelin@gmail.com>
2023-11-20 20:56:36 +0800

    Updated task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.getElementsByTagName('body');
 
+let timerId = null;
+
+startBtn.disabled = false;
+stopBtn.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    const hexColor = getRandomHexColor();
+    document.body.style.backgroundColor = hexColor;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
+});
